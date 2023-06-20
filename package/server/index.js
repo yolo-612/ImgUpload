367afa316bc649a9b242c6d1d3355a97267dc24d
keruisiya <2458097476@qq.com>
2023-06-20 23:16:05 +0800

    project init

---
+++ package/server/index.js
@@ -0,0 +1,14 @@
+const dotenv = require('dotenv').config();
+const express = require("express");
+
+const app = express();
+
+const PORT = process.env.PORT;
+
+app.get("/", (req, res) => {
+  res.send("Hello World");
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running at http://localhost:${PORT}`);
+});
