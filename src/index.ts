0a1f75addb38b0728c382c1df299abfda433e2a7
keruisiya <2458097476@qq.com>
2023-07-02 19:21:01 +0800

    添加nodemon监听

--- src/index.ts
+++ src/index.ts
@@ -52,6 +52,32 @@ app.post('/api/finishUpload', async (req: Request, res: Response) => {
   res.json({ path: suffix });
 })
 
+
+
+// ----纯文件上传作用
+app.post('/api/uploadFirst', upload.single('f1'), async (req:Request, res:  Response)=>{
+  console.log(req.files)
+  console.log(req)
+  // var file = ctx.request.files.f1;//得道文件对象
+  // var path = file.path;
+  // var fname = file.name;//原文件名称
+  // var nextPath = path+fname;
+  // if(file.size>0 && path){
+  //     //得到扩展名
+  //     var extArr = fname.split('.');
+  //     var ext = extArr[extArr.length-1];
+  //     var nextPath = path+'.'+ext;
+  //     //重命名文件
+  //     fs.renameSync(path, nextPath);
+  // }
+  // //以 json 形式输出上传文件地址
+  // ctx.body = `{
+  //     "fileUrl":"${uploadHost}${nextPath.slice(nextPath.lastIndexOf('/')+1)}"
+  // }`;
+})
+
+
+
 // 4. 调用 app.listen 来启动 server 并监听指定端口，启动成功后打印出 log
 app.listen(port, () =>
   console.log(`Express server listening at http://localhost:${port}`)
